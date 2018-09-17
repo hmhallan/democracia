04518bf7910b8232395f0f963f760f91784049cc
Hallan Medeiros <hallan.medeiros@tjsc.jus.br>
2018-09-17 18:57:52 -0300

    implementação da tela de listagem de propostas

--- src/modules/democracia/lista.propostas.controller.js
+++ src/modules/democracia/lista.propostas.controller.js
@@ -8,10 +8,20 @@
             //implementar aqui a busca de propostas cadastradas
 
             //1 - buscar (via api) o total de propostas
-            //esta funcionalidade inclusive ja esta implementada na tela de cadastro de propostas (é exibido no rodapé)
+            $scope.getTotalPropostas = function(){
+                api.proposta.getTotaldePropostas().then(function(retorno){
+                    $scope.totalDePropostas = retorno;
 
-            //2 - iterar o total de propostas, buscando cada uma delas (através da api também) e adicionando na listagem
-            //ex: $scope.propostas.push( proposta );
+                    //2 - iterar o total de propostas, buscando cada uma delas (através da api também) e adicionando na listagem
+                    for ( var i = 0; i < $scope.totalDePropostas; i++ ){
+                        api.proposta.getProposta( i ).then(function(proposta){ 
+                            $scope.propostas.push(proposta);
+                        });
+                    }
+
+                });
+            }
+            $scope.getTotalPropostas(); 
         }
 
         $scope.listarPropostas();
