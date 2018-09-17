a4f361947c7ca7c7ad518e303b72548975180aea
Hallan Medeiros <hallan.medeiros@tjsc.jus.br>
2018-09-17 18:59:08 -0300

    refactoring da logica de busca de propostas

--- src/modules/democracia/lista.propostas.controller.js
+++ src/modules/democracia/lista.propostas.controller.js
@@ -8,20 +8,17 @@
             //implementar aqui a busca de propostas cadastradas
 
             //1 - buscar (via api) o total de propostas
-            $scope.getTotalPropostas = function(){
-                api.proposta.getTotaldePropostas().then(function(retorno){
-                    $scope.totalDePropostas = retorno;
+            api.proposta.getTotaldePropostas().then(function(retorno){
+                $scope.totalDePropostas = retorno;
 
-                    //2 - iterar o total de propostas, buscando cada uma delas (através da api também) e adicionando na listagem
-                    for ( var i = 0; i < $scope.totalDePropostas; i++ ){
-                        api.proposta.getProposta( i ).then(function(proposta){ 
-                            $scope.propostas.push(proposta);
-                        });
-                    }
+                //2 - iterar o total de propostas, buscando cada uma delas (através da api também) e adicionando na listagem
+                for ( var i = 0; i < $scope.totalDePropostas; i++ ){
+                    api.proposta.getProposta( i ).then(function(proposta){ 
+                        $scope.propostas.push(proposta);
+                    });
+                }
 
-                });
-            }
-            $scope.getTotalPropostas(); 
+            }); 
         }
 
         $scope.listarPropostas();
